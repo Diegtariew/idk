5751a015b2c2f00a5ca1aff35b0e75a5fc2597a7
Diegtariew <maciej.harasim96@gmail.com>
2017-10-05 19:22:52 +0200

    XDD

--- main.cpp
+++ main.cpp
@@ -29,6 +29,42 @@ int main() {
     XD jakityp2;
     XD jakityp3;
 
+    int i = 1;
+    int &huj = i;
+
+    cout << "i: " << huj << endl;
+
+    int* wsk = &i;
+
+    cout << "i: " << *wsk << endl;
+
+
+    int x = 5;
+
+    int* tab = new int[x];
+    int* gowno = new int;
+    int* segz = new int;
+
+    for (int i = 0; i < x; i ++){
+        tab[i] = i + 1;
+    }
+
+    *gowno = 5;
+    *segz = 88;
+
+    cout << "gowno: " << *gowno << endl;
+    cout << "tab[3]: " << tab[3] << endl;
+
+    tab[3] = 18;
+    *gowno = tab[3];
+    cout << "gowno: " << *gowno << endl;
+
+    *gowno = *segz;
+    cout << "gowno: " << *gowno << endl;
+
+    gowno = segz;
+    cout << "gowno: " << *gowno << endl;
+
 
     return 0;
 }
